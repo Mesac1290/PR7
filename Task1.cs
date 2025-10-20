83bec75dabfda3d2878daa5730f999eee02159e4
Mesac1290 <suharevegor0@gmail.com>
2025-10-21 01:12:39 +0300

    Task1

---
+++ Task1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR7_task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string txt;
+            while (true)
+            {
+                Console.Write("Введите строку: ");
+                txt = Console.ReadLine();
+                if (string.IsNullOrWhiteSpace(txt))
+                {
+                    Console.WriteLine("Ошибка: некоректное значение");
+                    continue;
+                }
+                break;
+            }
+            string[] txtArray = txt.Split((char[]) null, StringSplitOptions.RemoveEmptyEntries);
+            Console.WriteLine($"Строка состоит из \"{txtArray.Length}\" подстрок");
+            for (int i = 0; i < txtArray.Length; i++)
+            {
+                Console.Write($"Подстрока: \"{txtArray[i]}\" имеет длину \"{txtArray[i].Length}\"\n");
+            }
+        }
+    }
+}
